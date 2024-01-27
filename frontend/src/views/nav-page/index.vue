b62b892ae0874ecc02076c2c3ee701dc2c05bdad
HiagoCS <hiago8cs@gmail.com>
2024-01-27 12:59:56 -0300

    bug de logo ajustado + botão de loggout

--- frontend/src/views/nav-page/index.vue
+++ frontend/src/views/nav-page/index.vue
@@ -4,8 +4,8 @@ export default{
   props:['items'],
   computed:{
     logo(){
-      if(this.$root.isLoggedIn) return './png/default_user.png';
-      else return './png/mb_logo.png';
+      if(this.$root.isLoggedIn) return {route:'/home/profile/me'};
+      if(!this.$root.isLoggedIn) return {route:"/home"};
       
     }
   }
@@ -17,7 +17,10 @@ export default{
       <div class="d-flex flex-column col-12 nav-main">
         <div class="d-flex flex-row col-12 justify-content-between nav-header">
           <div class="col-6 logo-div">
-            <RouterLink to="/"><img :src="logo"></RouterLink>
+            <RouterLink :to="logo.route">
+              <img v-if="this.$root.isLoggedIn" src="@/assets/png/default_user.png">
+              <img v-else src="@/assets/png/mb_logo.png">
+            </RouterLink>
           </div>
           <div class="d-flex col-6 justify-content-end align-items-center config-div">
             <RouterLink to="/config"><font-awesome-icon class="config-icon" icon="cog"/></RouterLink>
@@ -26,6 +29,11 @@ export default{
         <div class="d-flex flex-column col-12 align-items-center nav-body">
           <div class="d-flex col-12 justify-content-center nav-items" v-for="nav in this.items " :key="nav">
             <RouterLink class="item" :to="nav.route"><h1>{{ nav.title }}</h1></RouterLink>
+          </div>
+        </div>
+        <div class="d-flex flex-column col-12 align-items-center nav-footer" v-if="this.$root.isLoggedIn">
+          <div class="d-flex col-12 justify-content-center nav-items" >
+              <div class="item" @click="this.$root.loggout"><h1>Sair</h1></div>
           </div>
         </div>
       </div>
