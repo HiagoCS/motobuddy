13210ef19a9f9febd749b2d34151969539e58af4
HiagoCS <hiago8cs@gmail.com>
2024-06-06 15:13:19 -0300

    add vue-i18n to help in messages translation

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,29 @@
 import './assets/main.css'
-
-import { createApp } from 'vue'
+import * as Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faCog, faCamera } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 import '@/api/axios.js';
+
+import { createI18n } from 'vue-i18n'
+import {pt_BR} from '@/components/locales/pt_BR'
+const languages ={
+    pt_BR
+}
+const i18n = createI18n({
+    locale:'pt_BR',
+    fallbackLocale:'pt_BR',
+    messages: languages
+})
+
+
 library.add(faCog, faCamera);
 
-const app = createApp(App).component("font-awesome-icon", FontAwesomeIcon)
+const app = Vue.createApp(App).use(i18n).component("font-awesome-icon", FontAwesomeIcon).use(VueSweetalert2)
 
 app.use(router)
 
